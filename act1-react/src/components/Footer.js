1397813973c9f868607e5cf0530af590b6fff2d9
leomorecco <leomorecco@gmail.com>
2023-12-22 14:58:43 -0500

    Actividad 1
    Actividad 1

---
+++ act1-react/src/components/Footer.js
@@ -0,0 +1,11 @@
+// Footer.js
+import React from "react";
+import "../Style/Footer.css"; 
+
+export const Footer = () => {
+    return (
+        <footer className="footerStyle">            
+            <p>© 2023 Todos los derechos reservados.</p>
+        </footer>
+    );
+}
